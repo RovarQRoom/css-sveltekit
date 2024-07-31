a555f3b09421e2cfce6296b4454baa387789ce55
RovarQRoom <rovar.07000120@outlook.com>
2024-07-31 10:53:08 +0300

    chore: Update CSS variables and remove unused code

---
+++ src/routes/slider/+page.svelte
@@ -0,0 +1,27 @@
+<main>
+	<section class="carousel">
+		<div class="list">
+			<article class="item">
+				<div class="main-content">
+					<div class="content">
+						<h2>Coffee</h2>
+						<p class="price">$ 20</p>
+						<p class="description">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, tempore quos, placeat
+							totam ut animi magni voluptas modi quidem facere dolorum aliquid officiis, doloremque
+							aliquam. Sit maxime voluptatum quam ex.
+						</p>
+						<button class="addToCard"> Add To Card </button>
+					</div>
+				</div>
+				<figure class="image">
+                    <img src="images/slider/1.png" alt="" srcset="">
+                </figure>
+			</article>
+		</div>
+		<div class="arrows">
+			<button id="prev"></button>
+			<button id="next"></button>
+		</div>
+	</section>
+</main>
